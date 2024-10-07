bab91e893b403dcbf36e144fde483b8e1ddd4ef5
NikitaSelin <nikiselin3@gmail.com>
2024-10-08 00:17:36 +0300

    Changed DockerfileJenkins and added locale

--- create_env.sh
+++ create_env.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 touch ansible-password.txt
 echo "$VAULT_CREDS_PSW" > ansible-password.txt
-ansible-playbook setup.yml --vault-password-file ansible-password.txt
+LC_ALL="C.UTF-8" ansible-playbook setup.yml --vault-password-file ansible-password.txt
